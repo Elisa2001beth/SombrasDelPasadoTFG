b82da07ea3125370ed4efe504d8bb01cdf49af26
Elisa2001beth <90263330+Elisa2001beth@users.noreply.github.com>
2024-05-13 00:36:48 +0200

    sprites

--- SombrasDelPasado/Assets/Scripts/EnemyFollowPlayer.cs
+++ SombrasDelPasado/Assets/Scripts/EnemyFollowPlayer.cs
@@ -7,15 +7,33 @@ public class EnemyFollowPlayer : MonoBehaviour
 {
     public NavMeshAgent enemy;
     public Transform Player;
+    public Animator animator; // Referencia al componente Animator
+
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>(); // Obtener referencia al Animator
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Establecer la posición de destino del enemigo
         enemy.SetDestination(Player.position);
+
+        // Determinar si el enemigo está en movimiento
+        bool isMoving = enemy.velocity.magnitude > 0.1f;
+
+        // Si el enemigo está en movimiento, reproducir la animación de caminar
+        if (isMoving)
+        {
+            animator.SetTrigger("Walk");
+        }
+        else // Si el enemigo no está en movimiento, reproducir la animación de idle
+        {
+            animator.SetTrigger("Idle");
+        }
+
+        Debug.Log(isMoving);
     }
 }
